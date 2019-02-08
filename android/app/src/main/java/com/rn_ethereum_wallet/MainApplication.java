3620992b817901f9a8ac5354a9368289616486cd
OJS in Nexg <markan@nexl.kr>
2019-02-08 10:05:09 +0900

    add react-native-qrcode-svg

--- android/app/src/main/java/com/rn_ethereum_wallet/MainApplication.java
+++ android/app/src/main/java/com/rn_ethereum_wallet/MainApplication.java
@@ -3,6 +3,7 @@ package com.rn_ethereum_wallet;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.reactlibrary.securekeystore.RNSecureKeyStorePackage;
 import com.bitgo.randombytes.RandomBytesPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
             new RNSecureKeyStorePackage(),
             new RandomBytesPackage(),
             new RNGestureHandlerPackage()
--- src/components/WalletComponent.js
+++ src/components/WalletComponent.js
@@ -7,33 +7,33 @@ export default function WalletComponent(props) {
 	const wallet = props.wallet;
 	return (
 		<TouchableOpacity  onPress={props.onPress}>
-		<Card>
-			<CardItem>
-				<Left>
-					<Thumbnail small source={{uri: 'https://s2.coinmarketcap.com/static/img/coins/32x32/1027.png'}} />
-					<Body>
-						<Text>ETH</Text>
-						<Text note>{wallet.name}</Text>
+			<Card>
+				<CardItem>
+					<Left>
+						<Thumbnail small source={{uri: 'https://s2.coinmarketcap.com/static/img/coins/32x32/1027.png'}} />
+						<Body>
+							<Text>ETH</Text>
+							<Text note>{wallet.name}</Text>
+						</Body>
+					</Left>
+					<Right>
+						<Icon name='dots-vertical' type='MaterialCommunityIcons' />
+					</Right>
+				</CardItem>
+				<CardItem>
+					<Text note ellipsizeMode="middle" numberOfLines={1} selectable={true}>{wallet.address}</Text>
+				</CardItem>
+				<CardItem>
+					<Body style={{ alignItems:'flex-end' }}>
+						<Text>
+							{wallet.balance || '0.00'}
+						</Text>
+						<Text note style={{ marginRight:0 }}>
+							≈ ￦ {wallet.convertPrice || '0.00'}
+						</Text>
 					</Body>
-				</Left>
-				<Right>
-					<Icon name='dots-vertical' type='MaterialCommunityIcons' />
-				</Right>
-			</CardItem>
-			<CardItem>
-				<Text note ellipsizeMode="middle" numberOfLines={1} selectable={true}>{wallet.address}</Text>
-			</CardItem>
-			<CardItem>
-				<Body style={{ alignItems:'flex-end' }}>
-					<Text>
-						{wallet.balance || '0.00'}
-					</Text>
-					<Text note style={{ marginRight:0 }}>
-						≈ ￦ {wallet.convertPrice || '0.00'}
-					</Text>
-				</Body>
-			</CardItem>
-		</Card>
+				</CardItem>
+			</Card>
 		</TouchableOpacity>
 	);
 }
