bd07faca387c3fb47b982e9eea9e0c6d4200b625
안피곤 <markan82@gmail.com>
2019-02-14 23:23:12 +0900

    config android

--- android/app/src/main/java/com/rn_ethereum_wallet/MainApplication.java
+++ android/app/src/main/java/com/rn_ethereum_wallet/MainApplication.java
@@ -3,7 +3,6 @@ package com.rn_ethereum_wallet;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.horcrux.svg.SvgPackage;
 import com.reactlibrary.securekeystore.RNSecureKeyStorePackage;
 import com.bitgo.randombytes.RandomBytesPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -27,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new SvgPackage(),
             new RNSecureKeyStorePackage(),
             new RandomBytesPackage(),
             new RNGestureHandlerPackage()
