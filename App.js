6356c73ebfac9c405451c4be2db3d8365b6ab2ef
안피곤 <markan82@gmail.com>
2019-02-17 00:49:00 +0900

    add redux #2

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import thunk from 'redux-thunk'
+import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import allReducers from './src/reducers';
