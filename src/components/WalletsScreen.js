f1cc1b0c53defc870006f8708f7f95d6996530dc
안피곤 <markan82@gmail.com>
2019-02-17 00:42:01 +0900

    add redux

--- src/components/WalletsScreen.js
+++ src/components/WalletsScreen.js
@@ -8,7 +8,7 @@ import { NavigationEvents } from 'react-navigation';
 
 import { ethers } from 'ethers';
 
-export default class WalletsScreen extends Component {
+class WalletsScreen extends Component {
   static navigationOptions = {
     // headerLeft: null,
 		// title: "Ethereum Wallet",
@@ -25,41 +25,41 @@ export default class WalletsScreen extends Component {
 	}
 
 	componentWillMount() {
-		let provider = ethers.getDefaultProvider('ropsten');
+		// let provider = ethers.getDefaultProvider('ropsten');
 		
-		const pollingInterval = 20 * 1000;
-		this.poller = setInterval(() => {
-			const wallets = [...this.state.wallets];
-			wallets.forEach(wallet => {
-				provider.getBalance(wallet.address).then((balance) => {
-					const etherString = ethers.utils.formatEther(balance);
-					wallet.balance = etherString;
-				});
-			});
+		// const pollingInterval = 20 * 1000;
+		// this.poller = setInterval(() => {
+		// 	const wallets = [...this.state.wallets];
+		// 	wallets.forEach(wallet => {
+		// 		provider.getBalance(wallet.address).then((balance) => {
+		// 			const etherString = ethers.utils.formatEther(balance);
+		// 			wallet.balance = etherString;
+		// 		});
+		// 	});
 
-			// console.log(wallets)
+		// 	// console.log(wallets)
 
-			this.setState({ wallets }, () => {
-				AsyncStorage.setItem('WALLETS', JSON.stringify(wallets));
-			});
+		// 	this.setState({ wallets }, () => {
+		// 		AsyncStorage.setItem('WALLETS', JSON.stringify(wallets));
+		// 	});
 
-		}, pollingInterval);
+		// }, pollingInterval);
 	}
 
 	componentWillUnmount() {
-		if(this.poller) {
-			clearInterval(this.poller);
-		}
+		// if(this.poller) {
+		// 	clearInterval(this.poller);
+		// }
 	}
 
-	_onWillFocus = payload => {
-		// Storage 에서 지갑 목록 가져오기 // TODO: redux로 변경 필요?
-		AsyncStorage.getItem('WALLETS').then(wallets => {
-			this.setState({
-				wallets: JSON.parse(wallets) || [],
-			})
-		});
-	}
+	// _onWillFocus = payload => {
+	// 	// Storage 에서 지갑 목록 가져오기 // TODO: redux로 변경 필요?
+	// 	AsyncStorage.getItem('WALLETS').then(wallets => {
+	// 		this.setState({
+	// 			wallets: JSON.parse(wallets) || [],
+	// 		})
+	// 	});
+	// }
 
   render() {
     return (
@@ -76,7 +76,8 @@ export default class WalletsScreen extends Component {
 					</Header>
 					<Content padder>
 						{
-							this.state.wallets.map((wallet) => {
+							// this.state.wallets.map((wallet) => {
+							this.props.wallets.map((wallet) => {
                 return (
 									<WalletComponent 
 										key={wallet.address}
@@ -120,3 +121,22 @@ const styles = StyleSheet.create({
 		backgroundColor: 'white'
   },
 });
+
+// props에 전달할 state값 정의
+const mapStateToProps = (state) => {
+	const { wallets } = state.wallet;
+	return {
+		wallets
+	}
+};
+
+// props에 전달할 액션 함수 정의
+const mapDispatchToProps = { 
+	getWallets 
+};
+
+// 컴포넌트와 리덕스를 연결
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(WalletsScreen);
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import wallet from './walletReducer';
+
+export default combineReducers({
+  wallet: wallet
+});
